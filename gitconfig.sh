a89c67090bf06e24ff588b6d283d5b7a1451b434
Albin Olsson <albin.olsson@volvocars.com>
2013-12-04 09:52:32 +0100

    Add script for setting git global config

---
+++ gitconfig.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Git configs that I want everywhere I work
+
+# Correct line endings before pushing
+git config --global core.autocrlf input
+
+# Always show untracked files
+git config --global status.showuntrackedfiles all
+
+# Don't push all branches
+git config --global push.default tracking
+
+# Nice diffstat from pull/rebase
+git config --global rebase.stat true
+
+# Nice colors
+git config --global color.ui auto
+git config --global core.pager "less -FRSX"
