dcbc4c46d7db98b42a753a24dbdfb91622ace318
Albin Olsson <albin.olsson@gmail.com>
2014-12-20 18:21:21 +0100

    add reminder

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -2,6 +2,8 @@
 
 FILES=( inputrc vim vimrc bashrc bash_profile ackrc Xmodmap zdir tmux.conf haskeline ghci gitignore )
 
+# gnome-terminal-gconf.xml was created with the command
+# gconftool-2 --dump /apps/gnome-terminal/profiles > .dotfiles/gnome-terminal-gconf.xml
 gconftool-2 --load gnome-terminal-gconf.xml
 
 for FILE in ${FILES[@]}; do
